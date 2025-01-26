bff6339d358f5af7d478781ab34fdd73eaf64282
Ankit-yt <ankitpk303@gmail.com>
2025-01-27 01:56:07 +0530

    fifth commit

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,8 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    target: 'esnext', // Ensure modern JS syntax for browsers
-  },
+  base: "/PCTE-Hostel-Management"
   
 })
